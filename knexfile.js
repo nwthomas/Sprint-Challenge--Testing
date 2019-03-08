5169cff6fead9370a4928df21812a4f8129ff93f
Nathan Thomas <nwthomas@me.com>
2019-03-08 09:40:32 -0800

    Initialize with knex file and setup testing package.json scripts

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,28 @@
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./database/games.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }, // Command test is yarn test pre-setup with testing env
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
-
 };
